606ad426bf4ec4a54acbb0ab3cafa8c131ba8fce
Arman Kamal <53625839+ArmanKamal@users.noreply.github.com>
2020-06-12 12:23:15 -0700

    List & Dictionaries

---
+++ Day2.py
@@ -0,0 +1,26 @@
+### LISTS ###
+
+student_id = [12,13,14,15]
+students_result = [133,144,175,123]
+sum(students_result) ## Sum of the list
+len(students_result) ## Length of the list 
+students_result.append(31) ## Addiing a item to the end of the list ##
+# students_result.remove() ##
+
+student = [student_id,students_result]
+print(student)
+
+
+
+### Dictonaries ###
+
+my_data = {'name': 'Arman' , 'location': 'California'}
+print(my_data['name'])
+
+my_data.keys()
+
+## Adding Items to dictonary
+
+my_data['price'] = 0.50
+
+print(my_data)
