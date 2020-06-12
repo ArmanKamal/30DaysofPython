f1b8cd0151552bdb40d75976ef054593cb574c75
Arman Kamal <53625839+ArmanKamal@users.noreply.github.com>
2020-06-12 07:18:38 -0700

    Number,Variables,String

---
+++ Day1.py
@@ -0,0 +1,58 @@
+### Python is a dynamic language not statically type as you dont need to declare any variable before using them ###
+
+### There are two kinds of numbers in python 1.Integer 2.Float ###
+
+### Example of integer number ###
+
+int1 = 7
+print(int1) ## This will print 7 in the output  ##
+
+
+## Example of float number ##
+
+float1 = 7.0
+print(float1) ## So as you can see the decimal number is the float number ##
+
+
+## So we can do some Addtion & Substraction & Multiplication & Division & Remainder & expotential ##
+
+a = 6
+b = 4
+
+add = a+b
+sub = a-b
+mul = a*b
+div = a/b
+mod = a%b
+expo = a**b
+
+print(add)
+print(sub)
+print(mul)
+print(div)
+print(mod)
+print(expo)
+
+
+### Now string is number of characters  ###
+
+name = 'Arman'
+print(name)
+
+
+### If you multiply name by any number you will duplicate it many times ###
+print(name*5)
+
+### Now you cannot add int with string as they are not same kind of data types ###
+
+### The result that where we are  storing  are known as variables . you can easily store anything so that you can use it anywhere later###
+
+### Like the previous name variable contains a string so we can again re use it here ###
+print(name+' You are back again')
+
+
+### SO there you have it your day1 is done ###
+
+### Happy coding ###
+
+
